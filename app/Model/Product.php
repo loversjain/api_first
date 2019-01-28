a98ac93a8d13023194c4036e6564bdca58845b2f
Your Name <loversjain5@gmail.com>
2019-01-28 23:40:24 +0530

    update product by put method

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,6 +7,7 @@ use App\Model\Review;
 
 class Product extends Model
 {
+	protected $fillable = ['name', 'price', 'stock', 'discount', 'deatil'];
     public function reviews(){
     	return $this->hasMany(Review::class);
     }
